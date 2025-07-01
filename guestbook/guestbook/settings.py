dead92d7080e74af39a8610079b69a6c57ce3141
seoah <24seoah@sookmyung.ac.kr>
2025-07-01 18:57:05 +0900

    배포 환경 설정

--- guestbook/guestbook/settings.py
+++ guestbook/guestbook/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&&*#fwbvj@ir!netjo!hzsat%laz9wt(5zx$8=vy8%8o+t-6a9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
